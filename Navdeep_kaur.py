e8c73543215b3e4a562438ca4bce6e71fb25b128
navbrar0786 <91478200+navbrar0786@users.noreply.github.com>
2021-09-30 18:46:01 -0400

    IT support programming fumdamentals

---
+++ Navdeep_kaur.py
@@ -0,0 +1,18 @@
+item1 = input("please enter the price of the first item")
+item2 = input("please enter the price of the second item")
+item3 = input("please enter the price of the third item")
+item4 = input("please enter the price of the fourth item")
+
+subtotal= item1 + item2 + item3 + item4
+
+HST = 13%  subtotal
+
+total = subtotal + HST
+print = subtotal + HST
+
+#      calculation the cost of awesome shirts!
+
+#A customer in a store is purchasing two t-shirts of polo of black colors1
+# a customer in store is purchasing three shirts of orange color2
+# the price of each item is only $9.99
+# amount of HST and total
